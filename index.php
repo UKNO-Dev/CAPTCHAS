31f02a5ea6eaa2a5d4928a65ef465901811fa5b7
UKNO-Dev <raphael.carre08@gmail.com>
2018-12-12 15:06:25 +0100

    First commit
    First commit

--- index.php
+++ index.php
@@ -1,4 +1,39 @@
+
 <?php
-echo"<img src='script-captchas.php' alt='captchas' />"
+	session_start();
 ?>
 
+
+
+<?php
+
+echo "<img src='script-captchas.php' alt='captchas' />";
+
+
+ 
+if (!empty($_POST["cap"])) {
+    echo "<br>Code de sécurité doit contenir uen valeur !";    
+}else{  
+    echo "<br>Pas de valeur !!";
+}
+echo $_SESSION["code"];
+// var_dump($_POST);
+?>
+<!DOCTYPE html>
+<html>
+ <head>
+  <meta charset="UTF-8">
+</head> 
+    <body>
+
+
+<form action="index.php" method="post" >
+  <input type="text" name="cap">
+  <br>
+  <input type="submit" value="Submit">
+</form> 
+
+
+
+</body>
+</html>
--- script-captchas.php
+++ script-captchas.php
@@ -1,34 +1,60 @@
 <?php
 
-$chaine='23456789ABCDEFGHJKLMOPQRSTUVWXYZ';
-$image =imagecreatefrompng('Images/globalkek.png');
-$color =imagecolorallocate($image,255,0,0);
-$font ='Fonts/DRAGON.ttf';
 
-function getCode ($length, $chars) {
+session_start();
+
+
+
+$chaine = '23456789ABCDEFGHJKLMNPQRSTUVWXYZ';
+
+
+$image = imagecreatefrompng('Images/captcha_1.png');
+
+
+$color = imagecolorallocate($image, 140, 0, 140);
+
+
+$font = 'Fonts/Cartoon.ttf';
+
+
+function getCode($length, $chars) {
 	$code = null;
-	for ( $i=0; $i <$length; $i ++ )
-	{
-		$code .=$chars { mt_rand ( 0, strlen($chars) -1 ) };
-	}
+	for ( $i=0; $i < $length; $i++ )
+		{
+		$code .= $chars { mt_rand( 0, strlen($chars) - 1 ) };
+		}
 	return $code;
+	$_SESSION['test'] =$code;
+    echo $_SESSION['test'];
 };
-$code=getCode(5,$chaine);
 
-$char1=substr($code,0,1);
-$char2=substr($code,1,1);
-$char3=substr($code,2,1);
-$char4=substr($code,3,1);
-$char5=substr($code,4,1);
+//Creation de la variable de SESSION avec comme valeur $code
+$_SESSION["code"];
 
-imagettftext($image,80,-10,490,300,$color, $font, $char1);
-imagettftext($image,80,20,550,300,$color, $font, $char2);
-imagettftext($image,80,-35,610,300,$color, $font, $char3);
-imagettftext($image,80,25,670,300,$color, $font, $char4);
-imagettftext($image,80,-15,730,300,$color, $font, $char5);
+
+
+$code = getCode(5, $chaine);
+
+
+$char1 = substr($code,0,1);
+$char2 = substr($code,1,1);
+$char3 = substr($code,2,1);
+$char4 = substr($code,3,1);
+$char5 = substr($code,4,1);
+
+imagettftext($image, 28, -10, 0, 37, $color, $font, $char1);
+imagettftext($image, 28, 20, 37, 37, $color, $font, $char2);
+imagettftext($image, 28, -35, 55, 37, $color, $font, $char3);
+imagettftext($image, 28, 25, 100, 37, $color, $font, $char4);
+imagettftext($image, 28, -15, 120, 37, $color, $font, $char5);
+
+
 
 header('Content-Type: image/png');
+
 imagepng($image);
+
+
 imagedestroy($image);
-echo "aaaaaaaaaaaaaaaa"
+
 ?>
